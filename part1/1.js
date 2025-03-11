672db9fda48cacce7bf4d1d3f2226398bc7de656
swa1245 <swarajpatil2123@gmail.com>
2025-03-11 17:02:53 +0530

    done

---
+++ part1/1.js
@@ -0,0 +1,13 @@
+// var name = "John";
+// var age = 30;
+// console.log("My name is " + name + " and I am " + age + " years old");
+
+ 
+function count(){
+    var c =12
+    function a(){
+            console.log(++c)
+    }
+    a()
+}
+count()
